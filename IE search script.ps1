e327d1a720103bbefbd6242865d221ee8f131370
Nathanael88 <113363260+Nathanael88@users.noreply.github.com>
2022-09-12 11:30:30 -0500

    Add files via upload
    A PowerShell script used for searching a list in IE.

---
+++ IE search script.ps1
@@ -0,0 +1,11 @@
+﻿#Replace "$Searchfor" [string] with list of what you want to search
+$nav = 0x800
+$getpro = Get-Process iexplore
+$SearchFor = "Hawaiian Airlines","Southwest Airlines","Airbnb","Darktrace","Epic Systems","Autodesk","Microsoft","Amazon","Capgemini","Google"
+$IE = New-Object -ComObject "InternetExplorer.Application"
+foreach ($s in $SearchFor) {
+$IE.Visible = $true
+$IE.Navigate("https://www.google.com/search?q=$s+headquarters+location" , $nav)
+Start-Sleep -Seconds 1
+}
+Write-Host "Complete"
