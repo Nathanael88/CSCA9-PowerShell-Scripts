f8f7ed8178e92c9bdbbfe3d9f34aabd345e05f75
Nathanael88 <113363260+Nathanael88@users.noreply.github.com>
2022-09-13 11:15:02 -0400

    Disables user accounts not used in last 30 days
    Disables user accounts that have that have not been used to logon with in 30 or more days

---
+++ Scripts/Disable user accounts that have that have not been used to logon with in 30 or more days.ps1
@@ -0,0 +1,20 @@
+﻿#Script that searches for users who havn't logged in in the past 30 days
+
+$date = get-date
+Get-ADUser -f * -Properties LastLogonDate |
+
+#(-30) can be changed to adjust number of days
+
+where {$_.LastlogonDate -le $date.AddDays(-30)} |
+
+#Disable Account
+
+Disable-ADAccount
+
+#Writes results to a .txt
+
+Get-ADUser -f * -Properties LastLogonDate |
+where {$_.LastlogonDate -le $date.AddDays(-30)} |
+select name, ObjectGUID, Lastlogondate, enabled |
+Out-File -FilePath .\LastLogonDate_greater_than_30_days.txt
+Write-Host "Complete"
