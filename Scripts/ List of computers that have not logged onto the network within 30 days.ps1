51a3aec12774323aa5f45018ec6de24dbe7354e7
Nathanael88 <113363260+Nathanael88@users.noreply.github.com>
2022-09-13 15:19:25 -0400

    Computers not logged in for 30 days
     Creates a list of computers that have not logged onto the network within 30 days

---
+++ Scripts/ List of computers that have not logged onto the network within 30 days.ps1
@@ -0,0 +1,6 @@
+﻿#Script that generates .txt file of computers that have not logged on in the last 30 days
+$date = get-date
+Get-ADComputer -f * -Properties LastLogonDate |
+where {$_.LastLogonDate -le $date.AddDays(-30)} |
+Out-File .\Computer_LastLogonDate_greater_than_30_days.txt
+Write-Host "#### Complete ####"
