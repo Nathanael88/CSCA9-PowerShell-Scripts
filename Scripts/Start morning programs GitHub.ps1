913e376b95732e95f35e7c4e3c81649f07f555d6
Nathanael88 <113363260+Nathanael88@users.noreply.github.com>
2022-09-13 08:43:46 -0500

    Removed " () "and moved to scripts folder

---
+++ Scripts/Start morning programs GitHub.ps1
@@ -0,0 +1,2 @@
+#Replace XXX with the URLs for what sites you want to launch
+Start-Process -FilePath Chrome -ArgumentList 'XXX, XXX, XXX'
--- Start morning programs GitHub.ps1
+++
@@ -1,2 +0,0 @@
-#Replace XXX with the URLs for what sites you want to launch
-Start-Process -FilePath Chrome -ArgumentList '(XXX, XXX, XXX...)'
