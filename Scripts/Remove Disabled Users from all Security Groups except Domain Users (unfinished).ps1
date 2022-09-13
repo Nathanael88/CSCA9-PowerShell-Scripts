073b742ee833df9c465aa06e2cce06a274dcfaef
Nathanael88 <113363260+Nathanael88@users.noreply.github.com>
2022-09-13 13:31:40 -0400

    ***Not Done***
    Removes disabled users from all Security Groups except Domain Users (still needs work)

---
+++ Scripts/Remove Disabled Users from all Security Groups except Domain Users (unfinished).ps1
@@ -0,0 +1,8 @@
+﻿$users = Get-ADUser -f * -Properties enabled
+$SecurityGroups = Get-ADGroup -Filter {groupCategory -eq "security"} | where {$_.name -ne "Domain Guests" -or "Domain Users"}
+
+foreach ($u in $users) {
+    if ($u.enabled -eq $false) 
+    {Remove-ADPrincipalGroupMembership -Identity $u -MemberOf $SecurityGroups}
+}
+Write-Host "#### Complete ####"
