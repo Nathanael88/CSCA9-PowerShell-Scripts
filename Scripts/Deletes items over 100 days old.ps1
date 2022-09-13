a78e0bf2852b683fbe123796a9bcb0e8dd17252e
Nathanael88 <113363260+Nathanael88@users.noreply.github.com>
2022-09-13 15:46:09 -0400

    Deletes items older than 100 days old
    Script that will delete items in a folder that have a LastWriteTime greater than 100 days

---
+++ Scripts/Deletes items over 100 days old.ps1
@@ -0,0 +1,12 @@
+﻿#Deletes files that are older than 100 days
+$date = Get-Date
+
+###Replace XXX with file path###
+
+Get-ChildItem -Path XXX -Recurse |
+
+###Change (-100) to adjust number of days###
+
+Where-Object {$_.lastWriteTime -lt $date.AddDays(-100)} |
+Remove-Item
+Write-Host "####Complete####"
