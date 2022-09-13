7bfa22636e855e692ce029c8790ad40ea7236b05
Nathanael88 <113363260+Nathanael88@users.noreply.github.com>
2022-09-13 11:45:04 -0400

    Move disabled users into a specific OU
    Script that Moves disabled users into a Disabled_Users OU

---
+++ Scripts/Move disabled users into a specific OU.ps1
@@ -0,0 +1,20 @@
+﻿#Script that moves disabled user accounts into Disabled_Users OU
+
+### If Disabled_Users OU already Exisits skip to Step 2 ###
+
+# STEP 1 (Create Disabled_Users OU)
+
+New-ADOrganizationalUnit -Description "Disabled User Accounts" -Name Disabled_Users
+
+# STEP 2 (Move Disabled Users into Disabled_Users OU)
+### IMPORTANT #### 
+#Change TargetPath DC to new Domain
+
+$users = Get-ADUser -f * -Properties enabled
+$date = get-date
+
+foreach ($u in $users) {
+    if ($u.enabled -eq $false) 
+    {Move-ADObject -Identity $u -TargetPath "OU=Disabled_Users,DC=Adatum,DC=com"}
+}
+Write-Host "#### Complete ####"
