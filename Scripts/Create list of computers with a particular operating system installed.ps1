274404b3f4a82c83bf4bfebd2798bd95aa2a1fb5
Nathanael88 <113363260+Nathanael88@users.noreply.github.com>
2022-09-13 15:01:15 -0400

    Create list of computers with a particular operating system installed
    Creates a list of computers with a particular operating system installed

---
+++ Scripts/Create list of computers with a particular operating system installed.ps1
@@ -0,0 +1,8 @@
+﻿#Creates list of computers and thir operationg systems
+Invoke-Command -ComputerName (Get-ADComputer –Filter * | Select –ExpandProperty Name) -ScriptBlock {
+(Get-WMIObject win32_operatingsystem).name | 
+select (Get-WMIObject win32_operatingsystem).name
+} | 
+Format-List |
+Out-File -FilePath .\List_computer_operationg_systems.txt
+Write-Host "#### Complete ####"
