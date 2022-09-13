d73085994f50a571a9b8638a4c0d4db8d0a593a0
Nathanael88 <113363260+Nathanael88@users.noreply.github.com>
2022-09-13 10:41:54 -0400

    Retrieve all locked accounts script
    Script that creates a list of all AD user accounts that are locked out.

---
+++ Scripts/Retrieve all locked accounts.ps1
@@ -0,0 +1,5 @@
+﻿#Searches AD Users, retrives a list of locked out accounts and writes them to a .txt
+Search-ADAccount -LockedOut -UsersOnly | 
+Select-Object Name, GUID | 
+Out-File -FilePath .\Locked_Out_Users.txt
+Write-Host "Complete"
