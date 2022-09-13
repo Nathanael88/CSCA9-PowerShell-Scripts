34ac5b470826bb669ef2b3d3ac4bd9e157cb4f9f
Nathanael88 <113363260+Nathanael88@users.noreply.github.com>
2022-09-13 14:03:32 -0400

    Add Users into Groups
    Adds AD Users into Groups

---
+++ Scripts/Add Users into Groups.ps1
@@ -0,0 +1,4 @@
+﻿#Adds AD users to groups
+##Replace XXX with Group user is being added to
+##Replace yyy with user or users that are being added
+Add-ADGroupMember -Identity xxx -Members yyy -confirm
