76de8c803dc6a0db3ec74e760c812f5bd88561d6
Nathanael88 <113363260+Nathanael88@users.noreply.github.com>
2022-09-15 13:00:27 -0500

    Update Enumerate user accounts expired within last 24-hour period.ps1

--- Scripts/Enumerate user accounts expired within last 24-hour period.ps1
+++ Scripts/Enumerate user accounts expired within last 24-hour period.ps1
@@ -1,4 +1,4 @@
-#Creates list of users who's accounts are expired 
+#Creates list of users who's accounts expired in the last 24 hours.
 $date = Get-date
-Get-ADUser -f * -Properties AccountExpirationDate | select name,{$_.AccountExpirationDate -le $date} |
+Get-ADUser -f * -Properties AccountExpirationDate | select name,{$_.AccountExpirationDate -eq $date} |
 Out-File -FilePath .\Expired_Users_last24hrs.txt
