db219dba03cae64af830e0a1bdf61fb67e87aaa4
Nathanael88 <113363260+Nathanael88@users.noreply.github.com>
2022-09-15 12:58:57 -0500

    Changed -eq to -le
    Changed "-eq" to "-le" & added description

--- Scripts/Enumerate user accounts expired within last 24-hour period.ps1
+++ Scripts/Enumerate user accounts expired within last 24-hour period.ps1
@@ -1,3 +1,4 @@
-﻿$date = Get-date
-Get-ADUser -f * -Properties AccountExpirationDate | select name,{$_.AccountExpirationDate -eq $date} |
+#Creates list of users who's accounts are expired 
+$date = Get-date
+Get-ADUser -f * -Properties AccountExpirationDate | select name,{$_.AccountExpirationDate -le $date} |
 Out-File -FilePath .\Expired_Users_last24hrs.txt
